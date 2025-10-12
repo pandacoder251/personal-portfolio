25c38e55531ef9b89b0e92fed956b73185196ec4
pandacoder251 <pousalidolai59@gmail.com>
2025-10-12 19:02:39 +0530

    updated

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+
+  const mobileMenu = document.querySelector('.mobile-menu');
+  const mobileMenuBar = document.querySelector('.mobile-menu-bar');
+
+  mobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    mobileMenuBar.classList.toggle('active');
+  });
+
